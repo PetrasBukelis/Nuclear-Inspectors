6b8c7a3e02b945d4db05315286257e89b3c030e6
nastae <saauriscom>
2019-10-19 02:35:04 +0300

    Add initial backend app

---
+++ backend/src/main/java/org/spaceappschallenge/NuclearInspectorApplication.java
@@ -0,0 +1,13 @@
+package org.spaceappschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuclearInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuclearInspectorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/spaceappschallenge/NuclearInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spaceappschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuclearInspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
